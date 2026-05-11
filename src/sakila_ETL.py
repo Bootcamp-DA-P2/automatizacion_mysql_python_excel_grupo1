33d12300b4b8250d3362f01dbf717adb2ce2abc1
Noelia Sanchez facila <nsfacila@hotmail.com>
2026-05-11 12:37:22 +0200

    Feat:main.py and sakila_etl.py added

---
+++ src/sakila_ETL.py
@@ -0,0 +1,71 @@
+import pandas as pd
+
+from sqlalchemy import create_engine
+from pathlib import Path
+
+from src.config import (
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_NAME
+)
+
+# -----------------------------
+# CONEXIÓN MYSQL
+# -----------------------------
+
+engine = create_engine(
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+)
+
+# -----------------------------
+# RUTAS
+# -----------------------------
+
+QUERIES_PATH = Path("queries")
+OUTPUT_PATH = Path("output")
+
+OUTPUT_PATH.mkdir(exist_ok=True)
+
+# -----------------------------
+# EJECUTAR SQL
+# -----------------------------
+
+for sql_file in QUERIES_PATH.glob("*.sql"):
+
+    try:
+
+        print(f"\nEjecutando query: {sql_file.name}")
+
+        content = sql_file.read_text(encoding="utf-8")
+
+        queries = content.split(";")
+
+        query = [q.strip() for q in queries if q.strip()][-1]
+
+        query = query.replace("%", "%%")
+
+        with engine.connect() as connection:
+
+            df = pd.read_sql(query, connection)
+
+        print(f"DataFrame creado: {sql_file.stem}")
+
+        csv_name = sql_file.stem + ".csv"
+
+        output_file = OUTPUT_PATH / csv_name
+
+        df.to_csv(
+            output_file,
+            index=False,
+            encoding="utf-8-sig"
+        )
+
+        print(f"CSV generado: {csv_name}")
+
+    except Exception as e:
+
+        print(f"\n❌ Error en {sql_file.name}")
+        print(e)
+
+print("\nProceso ETL completado correctamente.")
